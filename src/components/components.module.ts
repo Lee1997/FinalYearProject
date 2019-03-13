09715e349e91614ced0ac28b92cdc1da32de238b
Lee1997 <leelaffan97@gmail.com>
2019-03-13 09:41:35 +0000

    Clean repo

--- src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { AccordionComponent } from './accordion/accordion';
-import { NavigationBarComponent } from './navigationBar/navigationBar';
-@NgModule({
-	declarations: [[AccordionComponent], [NavigationBarComponent]],
-	imports: [],
-	exports: [[AccordionComponent], [NavigationBarComponent]]
-})
-export class ComponentsModule {}
